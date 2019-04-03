2f2d001f1eeaa2343bd363d4f7a7f461be9121ca
andreyryzhkov <andrey.ryzhkov77@gmail.com>
2019-04-03 11:57:08 +0300

    [s-1] Fixed after review
    1) Session is immutable
    2) Session is Optional

--- src/main/java/com/aryzhkov/movieland/security/SecurityService.java
+++ src/main/java/com/aryzhkov/movieland/security/SecurityService.java
@@ -3,9 +3,11 @@ package com.aryzhkov.movieland.security;
 import com.aryzhkov.movieland.security.util.Session;
 import com.aryzhkov.movieland.web.util.Credential;
 
+import java.util.Optional;
+
 public interface SecurityService {
 
-    Session newSession(Credential credential);
+    Optional<Session> login(Credential credential);
 
-    void removeSession(String token);
+    void logout(String token);
 }
